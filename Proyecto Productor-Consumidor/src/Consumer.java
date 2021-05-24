9ffb194f5a66a83ff75ca84d5e14dd3b215d767f
Rivers <oscarrm96@hotmail.com>
2021-05-23 18:33:58 -0700

    Consumer API
    First commit

--- Proyecto Productor-Consumidor/src/Consumer.java
+++ Proyecto Productor-Consumidor/src/Consumer.java
@@ -1,31 +1,94 @@
 
+
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.*;
 
 public class Consumer extends Thread {
     Buffer buffer;
-    int cantidad;
-    
-    Consumer(Buffer buffer, int cantidad) {
+    private int waitMillis;
+    private volatile boolean Running = true;
+    DefaultTableModel consumed;
+
+    Consumer(Buffer buffer, int ms, DefaultTableModel consumed) {
         this.buffer = buffer;
-        this.cantidad = cantidad;
+        this.waitMillis = ms;
+        this.consumed = consumed;
     }
-    
+
     @Override
     public void run() {
-        System.out.println("Running Consumer...");
+        log("Running Consumer...");
         String product;
-        
-        for(int i=0 ; i<cantidad ; i++) {
+
+        while(Running) {
             product = this.buffer.consume();
             //System.out.println("Consumer consumed: " + product);
-            Buffer.print("Consumer consumed: " + product);
-            
+            int resultado_scheme = schemesolver(product);
+            Buffer.print("Consumer "+ this.getId()  + " consumed: " + product+" result scheme: "+resultado_scheme);
+            char operador = product.charAt(1);
+            int valor1 = Character.getNumericValue(product.charAt(3));
+            int valor2 = Character.getNumericValue(product.charAt(5));
+            Object[]rowData={operador,valor1,valor2,resultado_scheme};
+
+            if(valor2 == 0 && operador == '/'){
+                rowData[3]="INDT";
+            }
+
+            this.consumed.addRow(rowData);
+            //TODO: solve scheme operations
+
+
             try {
-                Thread.sleep(1000);
+                Thread.sleep(this.waitMillis);
             } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+                log("Stopped");
             }
         }
+    }
+
+    public int schemesolver(String operacion){
+        
+        char operador = operacion.charAt(1);
+
+        int valor1 = Character.getNumericValue(operacion.charAt(3));
+        int valor2 = Character.getNumericValue(operacion.charAt(5));
+
+        int result= 0;
+
+        switch(operador){
+            case '+':
+                result= valor1 + valor2;
+                break;
+            case '-':
+                result= valor1 - valor2;
+                break;
+            case '*':
+                result= valor1 * valor2;
+                break;
+            case '/':
+                if(valor2 == 0) {
+                    result = 0;
+                    log("indeterminado");
+                }
+
+                else result= valor1 / valor2;
+                break;
+
+
+        }
+
+        return  result;
+
+    }
+
+    public void terminate(){
+        Running = false;
+    }
+
+    private void log (Object obj){
+
+        System.out.println(obj);
     }
 }
